55a82eb796d036bcd3d8ac397a4d1523a6e67dba
sena <senika_@naver.com>
2023-04-03 11:03:57 +0900

    [#3]🎨HTML:sign페이지html

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import {useState} from 'react'
 
 const Signup = () => {
-  return <div>Signup</div>
+  const isUser = false
+  const [userInfo, setUserInfo] = useState(null)
+
+  const SignupHandler = () => {}
+  const SigninHandler = () => {}
+
+  return (
+    <div>
+      <form onSubmit={isUser ? SigninHandler : SignupHandler}>
+        <input data-testid="email-input" placeholder="email" name="email" />
+        <input data-testid="password-input" placeholder="password" name="password" />
+        {isUser ? (
+          <button data-testid="signin-button" type="submit">
+            로그인
+          </button>
+        ) : (
+          <button data-testid="signup-button" type="submit">
+            회원가입
+          </button>
+        )}
+      </form>
+    </div>
+  )
 }
 
 export default Signup
