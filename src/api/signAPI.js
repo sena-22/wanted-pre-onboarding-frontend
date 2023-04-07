298976d4291ff853780dbf1f8be71fa8c4342848
sena <senika_@naver.com>
2023-04-07 21:41:38 +0900

    [#4]api:로그인 성공 시 로직 추가

--- src/api/signAPI.js
+++ src/api/signAPI.js
@@ -1,5 +1,6 @@
-import {BASE_URL} from './const'
 import axios from 'axios'
+import {BASE_URL} from './const'
+import {saveTokenToLocalStorage} from '../utils/tokenHandler'
 
 export const signup = async args => {
   const signupRes = await axios.post(
@@ -22,8 +23,9 @@ export const signin = async args => {
       }
     )
     if (signupRes.status === 200) {
-      const loginRes = signupRes
-      return loginRes
+      const loginRes = signupRes.data
+      saveTokenToLocalStorage(loginRes.access_token)
+      return 'success'
     }
   } catch (err) {
     console.log(err)
