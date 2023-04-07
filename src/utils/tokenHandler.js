f68b1dfc7ba536a12e0ad277bc0d7fb7e228af60
sena <senika_@naver.com>
2023-04-07 21:43:43 +0900

    [#4]feat:토큰 관련 util 함수 추가 토큰 저장/조회 함수

---
+++ src/utils/tokenHandler.js
@@ -0,0 +1,7 @@
+export const saveTokenToLocalStorage = accessToken => {
+  localStorage.setItem('token', accessToken)
+}
+
+export const getTokenToLocalStorage = () => {
+  return localStorage.getItem('token') || ''
+}
