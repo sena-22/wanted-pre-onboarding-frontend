b2ee978eb64680ad989669dafe2e345cca6eb3c9
sena <senika_@naver.com>
2023-04-07 18:18:50 +0900

    [#3]feature:signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,24 +1,78 @@
 import React from 'react'
 import {useState} from 'react'
+import {signup} from '../api/signup'
 
 const Signup = () => {
   const isUser = false
-  const [userInfo, setUserInfo] = useState(null)
+  // const [userInfo, setUserInfo] = useState(null)
 
-  const SignupHandler = () => {}
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [emailValid, setEmailValid] = useState(false)
+  const [passwordValid, setPasswordValid] = useState(false)
+
+  const handleEmail = e => {
+    setEmail(e.target.value)
+    setEmailValid(emailValidationCheck(e.target.value) ? true : false)
+  }
+
+  const handlePassword = e => {
+    setPassword(e.target.value)
+    setPasswordValid(passwordValidationCheck(e.target.value) ? true : false)
+  }
+
+  const emailRegExp = /@/
+  const passwordRegExp = /.{8,}/
+
+  const emailValidationCheck = email => {
+    return emailRegExp.test(email) ? true : false
+  }
+
+  const passwordValidationCheck = password => {
+    return passwordRegExp.test(password) ? true : false
+  }
+
+  const SignupHandler = async e => {
+    e.preventDefault()
+    const signupResult = await signup({
+      email,
+      password
+    })
+    console.log(signupResult)
+  }
+
   const SigninHandler = () => {}
 
   return (
     <div>
       <form onSubmit={isUser ? SigninHandler : SignupHandler}>
-        <input data-testid="email-input" placeholder="email" name="email" />
-        <input data-testid="password-input" placeholder="password" name="password" />
+        <input
+          value={email}
+          data-testid="email-input"
+          placeholder="email"
+          name="email"
+          onChange={handleEmail}
+        />
+        <input
+          value={password}
+          data-testid="password-input"
+          placeholder="password"
+          name="password"
+          onChange={handlePassword}
+        />
         {isUser ? (
-          <button data-testid="signin-button" type="submit">
+          <button
+            data-testid="signin-button"
+            type="submit"
+            disabled={emailValid && passwordValid ? false : true}>
             로그인
           </button>
         ) : (
-          <button data-testid="signup-button" type="submit">
+          <button
+            data-testid="signup-button"
+            type="submit"
+            disabled={emailValid && passwordValid ? false : true}>
             회원가입
           </button>
         )}
