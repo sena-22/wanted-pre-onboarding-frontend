a92690c796d81bf110d8d4a22323ce79c87dbb77
sena <senika_@naver.com>
2023-04-07 23:18:23 +0900

    [#5]feat:todo 조회 기능 추가

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,30 +1,39 @@
 import React from 'react'
 import {useState, useEffect} from 'react'
-import {create_todo} from '../api/todoAPI'
+import {create_todo, get_todos} from '../api/todoAPI'
 
 const Todo = () => {
   const [todoList, setTodoList] = useState([])
   const [inputTodo, setInputTodo] = useState('')
 
-  // useEffect(() => {
-  //   //TODO: 서버에서 todo 목록 받아오기, 없을 때도 고려하기
-
-  // }, [])
+  useEffect(() => {
+    get_todos()
+      .then(todos => setTodoList(todos))
+      .catch(err => console.error(err))
+  }, [])
 
   const createTodo = async e => {
     e.preventDefault()
-    let newTodo = create_todo(inputTodo)
+    const newTodo = await create_todo(inputTodo)
     setTodoList(prev => [...prev, newTodo])
+    setInputTodo('')
   }
 
   const handleChangeInputTodo = e => {
     setInputTodo(e.target.value)
   }
 
+  const handleChecked = () => {}
+
   return (
     <>
       {/* 추가 */}
-      <input data-testid="new-todo-input" type="text" onChange={handleChangeInputTodo} />
+      <input
+        data-testid="new-todo-input"
+        type="text"
+        onChange={handleChangeInputTodo}
+        value={inputTodo}
+      />
       <button data-testid="new-todo-add-button" onClick={createTodo}>
         추가
       </button>
@@ -33,7 +42,11 @@ const Todo = () => {
         <div key={todo.id}>
           <li>
             <label>
-              <input type="checkbox" checked={todo.isCompleted} />
+              <input
+                type="checkbox"
+                checked={todo.isCompleted}
+                onChange={handleChecked}
+              />
               <span>{todo.todo}</span>
             </label>
           </li>
