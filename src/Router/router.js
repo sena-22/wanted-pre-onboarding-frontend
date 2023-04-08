1cd4e30f8508f5e947310b395eccb429fcd3000b
sena <senika_@naver.com>
2023-04-08 20:28:31 +0900

    [#2]setting:초기 라우팅 변경

--- src/Router/router.js
+++ src/Router/router.js
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <P.Sign type="signin" />,
+        element: <P.Sign type="signup" />,
         loader: SigninLoader
       },
       {
