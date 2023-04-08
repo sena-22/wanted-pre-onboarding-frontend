8aabb924b62497b36ea6d4e6521706eeb87f34bb
sena <senika_@naver.com>
2023-04-08 16:14:24 +0900

    [#3, #4]css:회원가입, 로그인 페이지 css 적용

--- src/pages/Sign.js
+++ src/pages/Sign.js
@@ -54,38 +54,48 @@ const Sign = ({type}) => {
   }
 
   return (
-    <div>
-      <form onSubmit={type === 'signin' ? SigninHandler : SignupHandler}>
-        <input
-          value={email}
-          data-testid="email-input"
-          placeholder="email"
-          name="email"
-          onChange={handleEmail}
-        />
-        <input
-          value={password}
-          data-testid="password-input"
-          placeholder="password"
-          name="password"
-          onChange={handlePassword}
-        />
-        {type === 'signin' ? (
-          <button
-            data-testid="signin-button"
-            type="submit"
-            disabled={emailValid && passwordValid ? false : true}>
-            로그인
-          </button>
-        ) : (
-          <button
-            data-testid="signup-button"
-            type="submit"
-            disabled={emailValid && passwordValid ? false : true}>
-            회원가입
-          </button>
-        )}
-      </form>
+    <div className="flex items-center justify-center w-screen h-screen font-mono text-zinc-100">
+      <div className="flex items-center justify-center w-2/6 bg-lime-900 rounded-xl h-3/6">
+        <form
+          onSubmit={type === 'signin' ? SigninHandler : SignupHandler}
+          className="flex flex-col items-center justify-center w-4/6 h-3/5">
+          <label className="mb-3">email</label>
+          <input
+            value={email}
+            data-testid="email-input"
+            placeholder="email"
+            name="email"
+            onChange={handleEmail}
+            className="w-full p-3 mb-5 text-orange-900 rounded h-1/6"
+          />
+          <label className="mb-3">password</label>
+          <input
+            value={password}
+            data-testid="password-input"
+            placeholder="password"
+            name="password"
+            onChange={handlePassword}
+            className="w-full p-3 mb-5 text-orange-900 rounded h-1/6 "
+          />
+          {type === 'signin' ? (
+            <button
+              data-testid="signin-button"
+              type="submit"
+              disabled={emailValid && passwordValid ? false : true}
+              className="w-full mt-3 bg-orange-600 rounded h-1/6">
+              sign in
+            </button>
+          ) : (
+            <button
+              data-testid="signup-button"
+              type="submit"
+              disabled={emailValid && passwordValid ? false : true}
+              className="w-full mt-3 bg-orange-600 rounded h-1/6">
+              sign up
+            </button>
+          )}
+        </form>
+      </div>
     </div>
   )
 }
